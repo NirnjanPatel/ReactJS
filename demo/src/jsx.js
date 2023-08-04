234b4fabcd995fc084ce97dc712940b048ec2a1d
nirnjan <devnirnjanpatel@gmail.com>
2023-08-04 13:52:30 +0530

    deleted

--- demo/src/jsx.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default function User() {
-  return <div>This is JSX</div>; // React.createElement('div',cl,React,.createElement) without jsx
-}
-// can we use angular and react both
-// Yes we can because react is an Library
